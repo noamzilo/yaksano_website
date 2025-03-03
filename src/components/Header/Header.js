e4c6006ee314721c2bf6ab5a415ff216af65b805
noam_wsl <noamzilo@gmail.com>
2025-03-03 18:13:54 -0500

    lint

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,16 @@
-import { header } from '../../portfolio'
 import Navbar from '../Navbar/Navbar'
 import './Header.css'
 import Logo from '../svgs/logo'
 
-const Header = () => {
-	const { homepage, title } = header
+const Header = () =>
+(
+	<header className='header'>
+		<div className='header__container center'>
+			<Logo />
+			<Navbar />
+		</div>
+	</header>
+)
 
-	return (
-		<header className='header'>
-			<div className='header__container center'>
-				<Logo />
-				<Navbar />
-			</div>
-		</header>
-	)
-}
 
 export default Header
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,9 @@
-import { useContext, useState } from 'react'
-import Brightness2Icon from '@material-ui/icons/Brightness2'
-import WbSunnyRoundedIcon from '@material-ui/icons/WbSunnyRounded'
+import { useState } from 'react'
 import MenuIcon from '@material-ui/icons/Menu'
 import CloseIcon from '@material-ui/icons/Close'
-import { ThemeContext } from '../../contexts/theme'
 import './Navbar.css'
 
 const Navbar = () => {
-	const [{ themeName, toggleTheme }] = useContext(ThemeContext)
 	const [showNavList, setShowNavList] = useState(false)
 
 	const toggleNavList = () => setShowNavList(!showNavList)
@@ -17,15 +13,6 @@ const Navbar = () => {
 			<a href='#contact' className='link link--nav'>
 				Contact Us
 			</a>
-
-			{/* <button
-				type='button'
-				onClick={toggleTheme}
-				className='btn btn--icon nav__theme'
-				aria-label='toggle theme'
-			>
-				{themeName === 'dark' ? <WbSunnyRoundedIcon /> : <Brightness2Icon />}
-			</button> */}
 
 			<button
 				type='button'
