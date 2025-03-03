8b25e47f1c5b85e728d636dfd0e7f96f8fedbece
noam_wsl <noamzilo@gmail.com>
2025-03-03 15:45:36 -0500

    made header with containers to be controllable

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,22 +3,24 @@ import Navbar from '../Navbar/Navbar'
 import './Header.css'
 
 const Header = () => {
-  const { homepage, title } = header
+	const { homepage, title } = header
 
-  return (
-    <header className='header center'>
-      <h3>
-        {homepage ? (
-          <a href={homepage} className='link'>
-            {title}
-          </a>
-        ) : (
-          title
-        )}
-      </h3>
-      <Navbar />
-    </header>
-  )
+	return (
+		<header className='header'>
+			<div className='header__container center'>
+				<h3>
+					{homepage ? (
+						<a href={homepage} className='link'>
+							{title}
+						</a>
+					) : (
+						title
+					)}
+				</h3>
+				<Navbar />
+			</div>
+		</header>
+	)
 }
 
 export default Header
