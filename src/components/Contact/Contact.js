5f669b766be2ac973dbb23e45e72485de9951529
noam_wsl <noamzilo@gmail.com>
2025-03-03 11:44:09 -0500

    contact content

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,12 +6,22 @@ const Contact = () => {
 
 	return (
 		<section className='section contact center' id='contact'>
-			<h2 className='section__title'>Contact</h2>
+			<h2 className='section__title'>Let’s Talk!</h2>
+			<p>
+				Have any questions or want to learn more about our AI and data solutions?
+				We’d love to hear from you! Reach out and let’s explore how we can help your business grow.
+			</p>
 			<a href={`mailto:${contact.email}`}>
 				<span type='button' className='btn btn--outline'>
-					Email us
+					Email Us
 				</span>
 			</a>
+
+			<div className='contact-info'>
+				<div>
+					<p><a href={`mailto:${contact.email}`}>{contact.email}</a></p>
+				</div>
+			</div>
 		</section>
 	)
 }
